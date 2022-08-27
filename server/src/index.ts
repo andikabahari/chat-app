ba9ff407e6660fde7cb1fe4fa54ba7511e67f4cf
Andika Bahari <andikabahari48@gmail.com>
2022-08-27 21:55:35 +0700

    Integrating socket.io

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,23 @@
 import express from 'express'
 import path from 'path'
+import http from 'http'
+import { Server } from 'socket.io'
 
 const app = express()
+const server = http.createServer(app)
 const port = process.env.PORT || 5000
+
+const io = new Server(server)
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname + '/../public/index.html'))
 })
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
+
+server.listen(port, () => console.log(`Server running on port ${port}`))
